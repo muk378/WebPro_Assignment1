64b1e43cb60eead04aec62ae44d29ac6d55300f1
muk378 <muk7859@hotmail.com>
2019-05-03 22:19:38 +0700

    edit menu bar

---
+++ assignment1/src/components/AddUser.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="adduser">
+    <h1>{{ title }}</h1>
+    <div class="vframe"> <!--vframe is a name that you crate by yourself-->
+    <form action=" ">
+        <div class="form-group">
+            <label class="pull-left">Firstname :</label>
+            <input type="text" class="form-control" v-model="User.firstName"
+            placeholder="First Name">
+        </div>
+        <div class="form-group">
+            <label class="pull-left">Lastname :</label>
+            <input type="text" class="form-control" v-model="User.lastName"
+            placeholder="Last Name">
+        </div>
+        <div class="form-group">
+            <label class="pull-left">E-mail :</label>
+            <input type="text" class="form-control" v-model="User.email"
+            placeholder="user@mail.com">
+        </div>
+        <button type="sumbit" class="btn-btn-large btn-block 
+        btn-primary full-width" @click="addToAPI">Submit</button>
+        <router-link to = '/users'>
+        <button type="sumbit" class="btn-btn-large btn-block 
+        btn-success full-width">Go to User Page</button>
+        </router-link>
+    </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'AddUser',
+  data () {
+    return {
+      title: 'Add User',
+      User: {
+        firstName: '',
+        lastName:'',
+        email:''
+      }
+    }
+  },
+  methods:{
+    addToAPI(){
+        let newUser={
+            firstName: this.User.firstName,
+            lastName: this.User.lastName,
+            email: this.User.email
+        }
+        console.log(newUser)
+        axios.post('http://localhost:3000/users',newUser)
+            .then((response)=>{
+                console.log(response)
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+            
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ assignment1/src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Users from '@/components/Users'
+import Contacts from '@/components/Contacts'
+import AddUser from '@/components/AddUser'
+import UpdateUser from '@/components/UpdateUser'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/hello',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/users',
+      name: '/Users',
+      component: Users
+    },
+    {
+      path: '/contacts',
+      name: '/Contacts',
+      component: Contacts
+    },
+    {
+      path: '/adduser',
+      name: '/AddUser',
+      component: AddUser
+    },
+    {
+      path: '/updateuser/:userId',
+      name: '/UpdateUser',
+      component: UpdateUser
+    }
+
+  ]
+})
